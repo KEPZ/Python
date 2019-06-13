e4d5dd3e6c4659c15230521b440cb1cec8578441
kepz <kepz@mail.ru>
2019-06-13 21:55:59 +0300

    script.py changed

--- script.py
+++ script.py
@@ -1,2 +1,8 @@
-import math
-print(f'The value of pi is approximately {math.pi:.3f}.')
+import os
+
+dict={'a':123,'b':456,'c':789}
+list=[1,3,6,2,9,5]
+tuple=('a','d', 2,'c','b', 5, 'e', 1)
+print(dict)
+print(list)
+print(tuple)
